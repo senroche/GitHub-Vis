4dc68ee38d5236a85ebe8e5dbe71c5f2277db716
senroche <35078186+senroche@users.noreply.github.com>
2019-11-06 17:16:39 +0000

    Adding logo and some styling to form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import LoginForm from './LoginForm.js';
 
+
 const octokit = require('@octokit/rest')()
 
 class App extends Component {
@@ -11,42 +12,47 @@ class App extends Component {
       username: '',
       password: '',
       submit: false,
+
     };
+    console.log(this.props);
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChanges = this.handleChanges.bind(this);
   }
 
-    handleSubmit(event) {
-      event.preventDefault();
-      console.log("trying to log in with username:", this.state.username, "Password:", this.state.password);
-      octokit.authenticate({username: this.state.username, password: this.state.password, type:'basic'});
-      octokit.repos.list().then(result => {console.log(result)});
-      this.setState({submit:true});
-    }
-  
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("trying to log in with username:", this.state.username, "Password:", this.state.password);
+    octokit.authenticate({username: this.state.username, password: this.state.password, type:'basic'});
+    octokit.repos.list().then(result => {console.log(result)});
+    this.setState({submit:true});
+  }
 
-    handleChanges(event) {
-      if(event.target.name==="username"){
-        this.setState({username: event.target.value});
-      }
-      else if(event.target.name === "password"){
-        this.setState({password: event.target.value});
-      }
-      else{
-        console.log('Trying to update:',event.target.name,'to',event.target.value);
-      }
+
+  handleChanges(event) {
+    if(event.target.name==="username"){
+      this.setState({username: event.target.value});
     }
+    else if(event.target.name === "password"){
+      this.setState({password: event.target.value});
+    }
+    else{
+      console.log('Trying to update:',event.target.name,'to',event.target.value);
+    }
+  }
+
+
+
+     
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <div>
-          {<LoginForm onChange={this.handleChanges} onSubmit={this.handleSubmit}/> }
+      <div>
+            
+              <LoginForm onChange={this.handleChanges} onSubmit={this.handleSubmit}/>
+            
           </div>
-        </header>
-      </div>
+        
     );
   }
 }
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+import logo from './img/logo.png';
+
 
 class LoginForm extends Component {
  
   render () {
     
     return (
-      <form className="login" onSubmit={this.props.onSubmit}>
-        <label htmlFor="username">GitHub Access</label>
-        <div>
-          <input type="username" required className="form-control" 
-            name="username"
-            placeholder="Username" 
-            value={this.props.username}
-            onChange={this.props.onChange}/>
-        </div>
-        <div>
-          <input type="password" className="form-control" 
-            name="password"
-            placeholder="Password"
-            value={this.props.password}
-            onChange={this.props.onChange}/>
-        </div>
-        <button type="submit" className="btn btn-primary">Log In</button>
-      </form>
+      <div className="App">
+        <header className="App-header">
+          <form className="login" onSubmit={this.props.onSubmit}>
+            <div className = "form-group">
+              <img src = {logo} alt = "logo" className="logo"/>
+              <h3>Log in to GitHub</h3>
+              <input type="username" required className="form-control" 
+              name="username"
+              placeholder="Username" 
+              value={this.props.username}
+              onChange={this.props.onChange}/>
+            </div>
+            <div>
+              <input type="password" className="form-control" id="lol"
+                name="password"
+                placeholder="Password"
+                value={this.props.password}
+                onChange={this.props.onChange}/>
+            </div>
+            <button type="submit" className="btn btn-primary">Log In</button>
+          </form>
+        </header>      
+      </div>
     )
   }
 }
