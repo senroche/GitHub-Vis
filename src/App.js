d7fdb88445219b6f52caaf83f576392ba20b8777
senroche <rochesean@gmail.com>
2019-11-06 14:57:33 +0000

    Moved auth to app.js and improved form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 import LoginForm from './LoginForm.js';
 
+const octokit = require('@octokit/rest')()
+
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      submit: false,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChanges = this.handleChanges.bind(this);
+  }
+
+    handleSubmit(event) {
+      event.preventDefault();
+      console.log("trying to log in with username:", this.state.username, "Password:", this.state.password);
+      octokit.authenticate({username: this.state.username, password: this.state.password, type:'basic'});
+      octokit.repos.list().then(result => {console.log(result)});
+      this.setState({submit:true});
+    }
+  
+
+    handleChanges(event) {
+      if(event.target.name==="username"){
+        this.setState({username: event.target.value});
+      }
+      else if(event.target.name === "password"){
+        this.setState({password: event.target.value});
+      }
+      else{
+        console.log('Trying to update:',event.target.name,'to',event.target.value);
+      }
+    }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <LoginForm />
+          <div>
+          {<LoginForm onChange={this.handleChanges} onSubmit={this.handleSubmit}/> }
+          </div>
         </header>
       </div>
     );
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,52 +1,28 @@
 import React, { Component } from 'react';
 import './style.css';
-const octokit = require('@octokit/rest')()
 
 class LoginForm extends Component {
-
-  constructor (props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: ''
-    };
-    this.handleUsername= this.handleUsername.bind(this);
-    this.handlePassword = this.handlePassword.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleUsername(event) {
-    this.setState({username: event.target.value});
-  }
-  handlePassword(event) {
-    this.setState({password: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    octokit.authenticate({username: this.state.username, password: this.state.password, type:'basic'})
-    octokit.repos.list().then(result => {console.log(result)});
-  }
-
-  errorClass(error) {
-    return(error.length === 0 ? '' : 'has-error');
-  }
-
+ 
   render () {
+    
     return (
-      <form className="login" onSubmit={this.handleSubmit}>
+      <form className="login" onSubmit={this.props.onSubmit}>
         <label htmlFor="username">GitHub Access</label>
         <div>
-          <input type="text" required className="form-control" name="username"
-            placeholder="Username"
-            onChange={this.handleUsername}/>
+          <input type="username" required className="form-control" 
+            name="username"
+            placeholder="Username" 
+            value={this.props.username}
+            onChange={this.props.onChange}/>
         </div>
         <div>
-          <input type="password" className="form-control" name="text"
+          <input type="password" className="form-control" 
+            name="password"
             placeholder="Password"
-            onChange={this.handlePassword}/>
+            value={this.props.password}
+            onChange={this.props.onChange}/>
         </div>
-        <button type="submit" className="btn btn-primary">Find Me</button>
+        <button type="submit" className="btn btn-primary">Log In</button>
       </form>
     )
   }
