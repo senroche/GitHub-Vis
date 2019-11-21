e41b2566876a22b632fb631866d602a599fa6cb7
senroche <rochesean@gmail.com>
2019-11-21 23:08:47 +0000

    Some style changes

--- src/components/Pie.js
+++ src/components/Pie.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from "d3";
+import '../style/style.css';
 
 class PiePath extends Component {
     constructor(props) {
--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import '../style/style.css';
 import Pie from './Pie.js'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import DonutChart from './Pie' ;
+import '../style/style.css';
 
 class User extends Component {
   
@@ -12,31 +11,28 @@ class User extends Component {
     return (
     <div>
       
-      <div className ="developer-stats">
-          <h3>Developer Stats</h3>
+      <div className ="quick-stats">
+          <h3 style ={{paddingBottom: "6px"}}>Quick Stats</h3>
         <div classname = "stat-box">
-            <h6> Public Repositories: <b>{this.props.info.public_repos}</b></h6>
-            <h6> Owned Private Repositories: <b> {this.props.info.owned_private_repos}</b></h6>
-            <h6> Public Gists: <b> {this.props.info.public_gists}</b></h6>
-            <h6> Private Gists: <b> {this.props.info.private_gists}</b></h6>
-        </div>   
-      </div> 
-      <div className = "social-stats">
-        <div classname = "stat-box">
-            <h3>Social Stats </h3>
-            <h6> Followers: <b>{this.props.info.followers}</b></h6>
-            <h6> Following: <b>{this.props.info.following}</b></h6>
-            <h6> Collaborators: <b>{this.props.info.collaborators}</b></h6>
+          
+          <h6 style ={{paddingBottom:"6px"}}><b>General</b></h6>
+          <h6> Public Repositories: <b>{this.props.info.public_repos}</b></h6>
+          <h6> Owned Private Repositories: <b> {this.props.info.owned_private_repos}</b></h6>
+          <h6> Public Gists: <b> {this.props.info.public_gists}</b></h6>
+          <h6> Private Gists: <b> {this.props.info.private_gists}</b></h6>
 
-        </div>
+          <h6 style ={{paddingBottom:"6px", paddingTop:"6px"}}><b>Social</b></h6>
+          <h6> Followers: <b>{this.props.info.followers}</b></h6>
+          <h6> Following: <b>{this.props.info.following}</b></h6>
+          <h6> Collaborators: <b>{this.props.info.collaborators}</b></h6>
+        </div>   
       </div>
-      <div className = "pie">
-      
+
+      <div className = "languageTile">
       <div>
                 <h3>Language Stats</h3>
-                <div className="section">
+                
                     <Pie data = {this.props.stats} width="500" height="300" id="bs_chart" padAngle={0.03}/>
-                </div>
             </div>
       </div>
       </div>
